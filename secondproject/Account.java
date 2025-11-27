35698df62fb3354b3202713d8d04a041891fcde2
sangamsnr <sangamsunar444@gmail.com>
2025-11-27 21:01:54 +0545

    Add files via upload

---
+++ secondproject/Account.java
@@ -0,0 +1,35 @@
+package secondproject;
+
+public class Account {
+    //instance variable
+    String acc_name;
+    String acc_type;
+    double balance;
+
+    public Account(String acc_name, String acc_type, double balance) {
+        this.acc_name = acc_name;
+        this.acc_type = acc_type;
+        this.balance = balance;
+    }
+
+    public double balanceEnquiry() {
+        return this.balance;
+    }
+
+    public void deposit(double amt){
+        this.balance +=amt;
+        System.out.println("Deposite successfull:New balance:"+ balanceEnquiry());
+    }
+
+    public void withdraw(double amt){
+        if(amt>this.balance) {
+            System.out.println("Sorry Insufficient balance");
+        }else{
+            this.balance -= amt;
+            System.out.println("Withdraw successfull: "+balanceEnquiry()) ;
+
+        }
+    }
+
+}
+
