163d07d90c7bd71dbf18908bb4ce4ba6e336debe
sangamsnr <sangamsunar444@gmail.com>
2025-11-27 20:57:44 +0545

    Add files via upload

---
+++ Swing/Swing.java
@@ -0,0 +1,8 @@
+package Swing;
+
+public class Swing {
+    public static void main(String[] args) {
+        new Calculator();
+
+    }
+}
---
+++ square/Main.java
@@ -0,0 +1,8 @@
+package square;
+
+public class Main {
+        public static void main(String[] args) {
+            new calculator2();
+        }
+    }
+
